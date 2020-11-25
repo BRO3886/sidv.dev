426ca75e5acbb1431ad980494d7fc9b83e268c4c
BRO3886 <siddverma1999@gmail.com>
2020-11-25 20:35:21 +0530

    :ambulance: fix typo

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
 				<link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
 				<meta name="viewport" content="initial-scale=1.0, width=device-width" />
 				<meta name="theme-color" content="#ffffff" />
-				<meta property="og:image" content="/asset/me.jpg"></meta>
+				<meta property="og:image" content="/assets/me.jpg"></meta>
 				<meta name="description" content="Developer. Designer. Technology Enthusiast." />
 				<title>Siddhartha Varma</title>
 			</Head>
