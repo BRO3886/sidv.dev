14e5c0a9cae9acb2a68cbbdbdcbd40d4e4ecdfca
BRO3886 <siddverma1999@gmail.com>
2020-11-25 19:24:12 +0530

    :bug: ci/cd fixes with tailwind not loading

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
 		removeDeprecatedGapUtilities: true,
 		purgeLayersByDefault: true,
 	},
-	purge: [],
+	purge: ["./components/**/*.{js,ts,jsx,tsx}", "./pages/**/*.{js,ts,jsx,tsx}"],
 	darkMode: false, // or 'media' or 'class'
 	theme: {
 		fontFamily: {
