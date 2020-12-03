3755f1b11c1e51c92503ed64574d5db05bfbf6ea
BRO3886 <siddverma1999@gmail.com>
2020-12-04 02:37:16 +0530

    :bento: changed some assets and footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,13 +3,14 @@ import React from "react";
 const Footer = () => {
 	return (
 		<div className="text-darkgrey py-3 bg-white">
-			<div className="flex justify-between w-11/12 mx-auto mt-1 items-center text-sm">
-				© {new Date().getFullYear()} | Siddhartha Varma
-				<div>
+			<div className="flex justify-center w-11/12 mx-auto mt-1 items-center text-sm">
+				© {new Date().getFullYear()} | 
+				<a href="https://github.com/BRO3886/" className="ml-1">Siddhartha Varma</a>
+				{/* <div>
 					<a href="https://github.com/BRO3886/siddharthavarma.tech">
 						<img src="/assets/contact/Github.svg" alt="github" className="h-5" />
 					</a>
-				</div>
+				</div> */}
 			</div>
 		</div>
 	);
