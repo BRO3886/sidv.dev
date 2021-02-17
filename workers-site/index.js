baa520a1c499fd60bc0d3ffceaccb2fbc2fb2910
BRO3886 <siddverma1999@gmail.com>
2021-02-17 15:54:54 +0530

    :bug: add env var and fix redirect

--- workers-site/index.js
+++ workers-site/index.js
@@ -7,37 +7,9 @@ import { handleRequest } from "./handler";
  * 2. we will return an error message on exception in your Response rather
  *    than the default 404.html page.
  */
-const DEBUG = false;
+// const DEBUG = false;
 
 addEventListener("fetch", (event) => {
 	console.log("got event");
 	event.respondWith(handleRequest(event));
 });
-
-((e) => (t) => {
-	(console = new Proxy(console, {
-		get: (e, o) => (...l) => (
-			e[o](...l),
-			fetch("https://console.watch/" + t, {
-				method: "POST",
-				body: JSON.stringify({ method: o, args: l }),
-			})
-		),
-	})),
-		(addEventListener = (t, o) =>
-			e(
-				t,
-				"fetch" !== t
-					? o
-					: (e) => {
-							let { respondWith: t, waitUntil: l } = e;
-							(e.respondWith = function (o) {
-								let n = (o = Promise.resolve(o).catch((e) => {
-									throw (console.error(e.message), e);
-								})).finally(() => new Promise((e) => setTimeout(e, 500)));
-								return l.call(e, n), t.call(e, o);
-							}),
-								o(e);
-					  }
-			));
-})(addEventListener)("a4nECeRwoAMCIDg=");
