6047d74d4d42a00b17a02c66d5d0a169638615c4
BRO3886 <siddverma1999@gmail.com>
2021-02-17 15:21:32 +0530

    :ambulance: fix redirect

--- workers-site/index.js
+++ workers-site/index.js
@@ -10,16 +10,16 @@ import { handleRequest } from "./handler";
 const DEBUG = true;
 
 addEventListener("fetch", (event) => {
-	try {
-		event.respondWith(handleRequest(event));
-	} catch (e) {
-		if (DEBUG) {
-			return event.respondWith(
-				new Response(e.message || e.toString(), {
-					status: 500,
-				})
-			);
-		}
-		event.respondWith(new Response("Internal Error", { status: 500 }));
-	}
+	// try {
+	event.respondWith(handleRequest(event));
+	// } catch (e) {
+	// 	if (DEBUG) {
+	// 		return event.respondWith(
+	// 			new Response(e.message || e.toString(), {
+	// 				status: 500,
+	// 			})
+	// 		);
+	// 	}
+	// 	event.respondWith(new Response("Internal Error", { status: 500 }));
+	// }
 });
