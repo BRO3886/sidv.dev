954b8419bff236d8e54410176bb15c44edf02fa8
Siddhartha Varma <siddverma1999@gmail.com>
2021-03-12 19:56:45 +0530

    :rocket: add redirects

--- workers-site/data.js
+++ workers-site/data.js
@@ -3,4 +3,5 @@ export const redirect = {
 	yt: "https://www.youtube.com/channel/UCTXiGXu9gecaKhq0GSG7-Kg",
 	"angel-hacks": "https://youtu.be/J18fI13uNIs",
 	medium:"https://medium.com/@siddverma1999",
+	lnkdn:"https://www.linkedin.com/in/siddharthav22/",
 };
