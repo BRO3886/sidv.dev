201a392f75ac5946e718a4a9f8fee29231e3675d
BRO3886 <siddverma1999@gmail.com>
2021-03-14 15:19:17 +0530

    :sparkles: added new resume

--- workers-site/data.js
+++ workers-site/data.js
@@ -2,8 +2,8 @@ export const redirect = {
 	old: "https://siddharthavarma.tech",
 	yt: "https://www.youtube.com/channel/UCTXiGXu9gecaKhq0GSG7-Kg",
 	"angel-hacks": "https://youtu.be/J18fI13uNIs",
-	medium:"https://medium.com/@siddverma1999",
-	lnkdn:"https://www.linkedin.com/in/siddharthav22/",
-	twt:"https://twitter.com/sidv_22",
-	riviera:"https://play.google.com/store/apps/details?id=com.riviera.riviera_app",
+	medium: "https://medium.com/@siddverma1999",
+	lnkdn: "https://www.linkedin.com/in/siddharthav22/",
+	twt: "https://twitter.com/sidv_22",
+	riviera: "https://play.google.com/store/apps/details?id=com.riviera.riviera_app",
 };
