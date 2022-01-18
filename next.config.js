53f74c7800599c84bd4c94c9134ae163081e8cba
Siddhartha Varma <siddverma1999@gmail.com>
2022-01-18 22:49:38 +0530

    fix: add cache control policy

--- next.config.js
+++ next.config.js
@@ -1,4 +1,37 @@
 module.exports = {
-  // Target must be serverless
-  target: "serverless",
+  target: 'serverless',
+  async headers() {
+    return [
+      {
+        source: '/(.*).webp',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value:
+              'public, max-age=180, s-maxage=180, stale-while-revalidate=180',
+          },
+        ],
+      },
+      {
+        source: '/(.*).png',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value:
+              'public, max-age=180, s-maxage=180, stale-while-revalidate=180',
+          },
+        ],
+      },
+      {
+        source: '/(.*).svg',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value:
+              'public, max-age=180, s-maxage=180, stale-while-revalidate=180',
+          },
+        ],
+      },
+    ];
+  },
 };
